d91fd47a3fd1fc1e69f75d3206d744995c2a63b1
villanelle9711 <164319454+villanelle9711@users.noreply.github.com>
2024-08-13 11:52:28 +0300

    add java-80

---
+++ java-80/main.js
@@ -0,0 +1,42 @@
+// // let i = 0
+
+// // while(i < 10) {
+// // i = i + 1
+// // console.log(i)
+// // }
+
+
+// // let i = 0
+
+// // do {
+// //     i++
+// //     console.log(i)
+// // } while (i < 5)
+
+// for (let i = 0; i < 10; i++) {
+//     console.log(`Пройден ${i} круг`)
+//     }
+
+// Домашкаааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааа
+
+// for (let i = 2; i <= 12; i += 2) {
+//     console.log(i);
+// }
+
+// for (let i = 10; i >= 1; i--) {
+//     console.log(i);
+// }
+
+
+// let i = 2;
+
+// while (i <= 12) {
+//     console.log(i);
+//     i += 2;
+// }
+
+let i = 10;
+while (i >= 1) {
+    console.log(i);
+    i--;
+}
